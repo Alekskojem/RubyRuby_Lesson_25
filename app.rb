0faf9c6f9879230d10a908bb3b56f9926270da31
alekskojem <alekskojem@gmail.com>
2017-08-30 21:06:03 +0300

    Add Users table sqlite3 by command line

--- app.rb
+++ app.rb
@@ -1,6 +1,46 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
-db = SQLite3::Database.new 'test.sqlite.sql'
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
 
-bd.close
+get '/About' do
+	@error = 'something wrong!'
+	erb :About
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	hh = { :username =>'ВВЕДИТЕ ИМЯ!', 
+		   :phone => 'ВВЕДИТЕ НОМЕР ТЕЛЕФОНА',
+		   :datetime => 'Укажите точную дату и время!' }
+	#для каждой пары ключ-значение
+	#hh.each do |key, value|
+	#if params[key] == ''
+		#переменной error присвоить value из хеша hh
+		#(a value из хеша hh это ссобщение об ошибке)
+		# переменной error присвоить сообщение об ошибке
+	#	@error = hh[key]
+		#вернуть предстивление visit
+	#	return erb :visit
+	#end
+	@error = hh.select  {|key,_| params[key] == ""}.values.join(", ")
+	if @erro != ''
+		return erb :visit
+	end
+	erb "OK, Имя посетителя #{@username}, телфон #{@phone}, указано время #{@datetime}, мастера #{@barber}, #{@color}."
+end
